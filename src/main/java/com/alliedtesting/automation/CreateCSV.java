5378241f57a623e9c022953a34f185ac9eaf01e3
Oleg Botezatu <obotezatu@gmail.com>
2019-02-18 23:52:54 +0200

    read-write csv

--- src/main/java/com/alliedtesting/automation/CreateCSV.java
+++ src/main/java/com/alliedtesting/automation/CreateCSV.java
@@ -1,11 +1,17 @@
 package com.alliedtesting.automation;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 
 import com.opencsv.CSVWriter;
 
@@ -14,30 +20,39 @@ public class CreateCSV {
 	public static void openCSVWriteData(String filePath) {
 		File file = new File(filePath);
 		List<String[]> data;
-		try{
+		try {
 			FileWriter outputFile = new FileWriter(file);
 			CSVWriter writer = new CSVWriter(outputFile);
 			data = inputRecords();
 			writer.writeAll(data);
 			writer.close();
-			
-		}catch(IOException e){
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	private static List<String[]>  inputRecords() {
+
+	public static void commonCSVWriteData(String filePath) throws IOException {
+		try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(filePath));
+				CSVPrinter csvPrinter = new CSVPrinter(writer,
+						CSVFormat.DEFAULT.withHeader("First name", "Last name", "University", "Score"));) {
+			List<String[]> data = inputRecords();
+			csvPrinter.printRecords(data);
+			csvPrinter.flush();
+		} 
+	}
+
+	private static List<String[]> inputRecords() {
 		String firstName;
 		String lastName;
 		String university;
 		String score;
-		//String[] student = new String[4];
 		List<String[]> data = new ArrayList<>();
 		System.out.println("List of students");
-		try(Scanner input = new Scanner(System.in)){
+		try (Scanner input = new Scanner(System.in)) {
 			boolean flag = true;
-			String next; 
+			String next;
 			while (flag) {
 				System.out.print("First Name: ");
 				firstName = input.nextLine();
@@ -47,10 +62,11 @@ public class CreateCSV {
 				university = input.nextLine();
 				System.out.print("Score: ");
 				score = input.nextLine();
-				data.add(new String[] {firstName, lastName, university, score});
+				data.add(new String[] { firstName, lastName, university, score });
 				System.out.print("Next? (y/n): ");
 				next = input.nextLine();
 				if (!"y".equals(next)) {
+					System.out.println("End input");
 					break;
 				}
 			}
